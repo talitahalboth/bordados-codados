d40b1418737d7b7665d25f6c66f33068743df046
Talita Halboth <talitahalboth@gmail.com>
2021-08-20 22:22:21 -0300

    fix images

--- src/Components/AnchorToDmc.tsx
+++ src/Components/AnchorToDmc.tsx
@@ -3,21 +3,8 @@ import { getAnchorColourList } from "../utils/anchorColour"
 import { getDmcColourList } from "../utils/dmcColour"
 import ColourWithImage from "./ColourWithImage"
 
-const ANCHOR_COLOUR_LIST = '../__fixtures__/coresAnchor/'
-// const DMC_COLOUR_LIST = '../__fixtures__/coresDmc/'
-// const DEFAULT_IMG = '../__fixtures__/coresDmc/1.jpg'
-
-// const fs = require('fs');
-
-
-// const fileExists = (file: string) => {
-//     try {
-//         fs.accessSync(file, fs.constants.R_OK | fs.constants.W_OK);
-//         return true;
-//     } catch (err) {
-//         return false;
-//     }
-// }
+import { anchorImages } from "../__fixtures__/coresAnchor"
+import { dmcImages } from "../__fixtures__/coresDmc"
 
 const AnchorToDmc = () => {
 
@@ -25,37 +12,31 @@ const AnchorToDmc = () => {
     const sorted = getAnchorAndDmcList()
     const dmcColourList = getDmcColourList()
     const anchorColourList = getAnchorColourList()
-    // const logo = require('../__fixtures__/coresDmc/01.jpg')
-    // console.log(logo)
-    // const logo = require('../__fixtures__/coresDmc/1.jpg')
-    // const imgs = sorted.map(({ anchor }) => {
-
-    //     const anchorImgPath = ANCHOR_COLOUR_LIST + anchor + '.jpg'
-    //     console.log(anchorImgPath)
-    //     // const anchorImgExists = fileExists(anchorImgPath) // OUTPUTS: true or false
-    //     // console.log(anchorImgPath, anchorImgExists)
-    //     const anchorImg = require(DEFAULT_IMG)
-
-    //     return anchorImg
-    // })
-    // const dmcImgPath = DMC_COLOUR_LIST + entry.dmc + 'jpg'
 
 
-    // const dmcImgExists = fileExists(dmcImgPath) // OUTPUTS: true or false
-    // const dmcImg = dmcImgExists ? require(DEFAULT_IMG) : require(DEFAULT_IMG)
+    const mapAnchor = anchorImages()
+    const mapDmc = dmcImages()
 
     return (<div style={{ width: '100%' }} >
         {
             sorted.map((entry, index) => {
 
-                // fileExists('your file name', (err, exists) => console.log(exists))
                 return (
                     <div key={index}>
-                        <ColourWithImage count={index} backgroundColour={anchorColourList.get(entry.anchor) ?? "white"} label="Anchor" colourName={entry.anchor} file={ANCHOR_COLOUR_LIST + entry.anchor + '.jpg'} />
-                        {/* <img src={String(logo.default)} /> */}
-                        <div style={{ backgroundColor: dmcColourList.get(entry.dmc ?? "white") }}>
-                            {"DMC: " + entry.dmc}
-                        </div>
+                        <ColourWithImage
+                            count={index}
+                            backgroundColour={anchorColourList.get(entry.anchor) ?? "white"}
+                            label="Anchor"
+                            colourName={entry.anchor}
+                            file={mapAnchor.get(entry.anchor)} />
+
+                        <ColourWithImage
+                            count={index}
+                            backgroundColour={dmcColourList.get(entry.anchor) ?? "white"}
+                            label="DMC"
+                            colourName={entry.dmc}
+                            file={mapDmc.get(entry.dmc)} />
+
                     </div>
                 )
 
--- src/Components/ColourWithImage.tsx
+++ src/Components/ColourWithImage.tsx
@@ -1,6 +1,6 @@
 
 interface Props {
-    file: string
+    file?: string
     label: string
     colourName: string
     backgroundColour: string
@@ -9,29 +9,33 @@ interface Props {
 
 // const DEFAULT_IMG = '../__fixtures__/coresDmc/1.jpg'
 
-const fs = require('fs');
+// const fs = require('fs');
 
 
-const fileExists = (file: string) => {
-    try {
-        fs.accessSync(file, fs.constants.R_OK | fs.constants.W_OK);
-        return true;
-    } catch (err) {
-        return false;
-    }
-}
+// const fileExists = (file: string) => {
+//     try {
+//         require(file);
+//         return true;
+//     } catch (err) {
+//         console.log(err)
+//         return false;
+//     }
+// }
 
-const ColourWithImage = ({ count, file, label, colourName, backgroundColour }: Props) => {
-    const logo = require('../__fixtures__/coresDmc/1.jpg')
-    if (count < 10 && fileExists(file)) console.log(file)
-    if (count < 10) console.log(file)
+
+const ColourWithImage = ({ file, label, colourName, backgroundColour }: Props) => {
+    // const logo = require('../__fixtures__/coresDmc/1.jpg')
+    // console.log(anchorImages.anchor1001)
+    // if (count < 10 && fileExists(file)) console.log(file)
+    // if (count < 10) console.log(file)
     // if (fileExists(DEFAULT_IMG)) console.log(DEFAULT_IMG)
+
     return (
-        <div>
-            <img alt="threadPicture" src={String(logo.default)} />
-            <div style={{ backgroundColor: backgroundColour }}>
+        <div style={{ display: 'flex', flexDirection: 'row', alignContent: "space-between", width: '100%', alignItems: "center" }}>
+            <div style={{ backgroundColor: backgroundColour, width: "100%" }}>
                 {`${label} ${colourName}`}
             </div>
+            {file && <img style={{ maxHeight: "50px", maxWidth: "50px" }} alt="threadPicture" src={file} />}
         </div>
     )
 
